e3c8162af17e1d64af8b94a84a258ba2d38fb844
Web Animate <60752454+webanimate@users.noreply.github.com>
2020-02-25 17:42:42 +0200

    feat: Sanitize and validate both properties names and values

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-export const options = {
+import { isObject, isEmpty, isArray, isString } from 'underscore'
+
+const properties = {
   id: {
     type: 'String'
   },
@@ -97,15 +99,103 @@ export const options = {
   }
 }
 
-export const sanitize = (obj, returnDefault = false) => {
-  if (returnDefault) {
+const propertiesNames = []
+Object.keys(properties).forEach(key => {
+  propertiesNames.push(key)
+})
+
+const isValidPropertyValue = (key, value) => {
+  const property = properties[key]
+  if (property) {
+    if (property.type === 'Number') {
+      if (!(value >= property.min && value <= property.max)) {
+        return false
+      }
+    } else if (property.type === 'String') {
+      if (property.values) {
+        value = value
+          .toString()
+          .replace(/\s+\(/g, '(')
+          .trim()
+        const bracketPosition = value.indexOf('(')
+        if (bracketPosition > 0) {
+          value = value.substring(0, bracketPosition + 1)
+        }
+        for (const propertyValue of property.values) {
+          if (value === propertyValue) {
+            return true
+          }
+        }
+        return false
+      }
+      return true
+    }
     return true
   }
-  const _options = {}
-  Object.keys(obj).forEach(function(key) {
-    if (key in options) {
-      _options[key] = obj[key]
-    }
-  })
-  return _options
+  return false
 }
+
+const sanitize = (obj, checkValue = true, returnDefault = true) => {
+  let _properties
+  if (isArray(obj)) {
+    _properties = []
+  } else if (isObject(obj)) {
+    _properties = {}
+  }
+  if (!isEmpty(obj)) {
+    if (!isArray(obj) && isObject(obj)) {
+      Object.keys(obj).forEach(key => {
+        if (key in properties) {
+          if (checkValue) {
+            if (!isValidPropertyValue(key, obj[key])) {
+              if (returnDefault && properties[key].default !== undefined) {
+                _properties[key] = properties[key].default
+              }
+            } else {
+              _properties[key] = obj[key]
+            }
+          } else {
+            _properties[key] = obj[key]
+          }
+        }
+      })
+    } else if (isArray(obj)) {
+      for (const key of obj) {
+        if (propertiesNames.includes(key)) {
+          _properties.push(key)
+        }
+      }
+      return _properties
+    }
+  }
+  return _properties
+}
+
+const validate = (obj, checkValue = true) => {
+  if (!isEmpty(obj)) {
+    if (!isArray(obj) && isObject(obj)) {
+      for (const key of Object.keys(obj)) {
+        if (!(key in properties)) {
+          return false
+        } else if (checkValue) {
+          if (!isValidPropertyValue(key, obj[key])) {
+            return false
+          }
+        }
+      }
+      return true
+    } else if (isArray(obj)) {
+      for (const key of obj) {
+        if (!propertiesNames.includes(key)) {
+          return false
+        }
+      }
+      return true
+    } else if (isString(obj) && propertiesNames.includes(obj)) {
+      return true
+    }
+  }
+  return false
+}
+
+export { properties, propertiesNames, sanitize, validate }
