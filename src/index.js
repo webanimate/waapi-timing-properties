74fce14155683a3d06cde240c412f3ea7b4fd0a1
Web Animate <60752454+webanimate@users.noreply.github.com>
2020-04-17 19:27:25 +0200

    feat: add valuesCubicBezier for `easing` property

--- src/index.js
+++ src/index.js
@@ -45,6 +45,13 @@ const properties = {
       'step-start',
       'step-end',
     ],
+    valuesCubicBezier: {
+      linear: '0.25, 0.25, 0.75, 0.75',
+      ease: '0.25, 0.1, 0.25, 1',
+      'ease-in': '0.42, 0, 1, 1',
+      'ease-out': '0, 0, 0.58, 1',
+      'ease-in-out': '0.42, 0, 0.58, 1',
+    },
     default: 'linear',
   },
 
